d822620cda877422dca0e910aad2877d73efc691
Jerilyn Yare <102701655+Jy-re@users.noreply.github.com>
2024-03-04 16:59:15 +0800

    fixed local data repo edit

--- src/app/redux/todo.action.ts
+++ src/app/redux/todo.action.ts
@@ -1,11 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+//LOCAL DATA IMPORT
 // import TaskRepositoryImpl from "../../data/repositories/TaskRepositoryImpl";
 import { TaskServiceImpl } from "../../domain/usecases/TaskUsecases";
+//FIREBASE DATA IMPORT
 import FirebaseTaskRepositoryImpl from "../../data/repositories/FirebaseTaskRepositoryImpl";
 import { Task } from "../../domain/entities/Task";
 
 //LOCAL DATA REPO
-//const taskDataRepos = new TaskRepositoryImpl();
+// const taskDataRepos = new TaskRepositoryImpl();
 
 //FIREBASE DATA REPO
 const taskDataRepos = new FirebaseTaskRepositoryImpl();
@@ -55,7 +57,7 @@ export const updateTask = createAsyncThunk(
   "tasks/updateTask",
   async ({ task }: { task: Task }, { rejectWithValue }) => {
     try {
-      const taskService = new TaskServiceImpl(new FirebaseTaskRepositoryImpl());
+      const taskService = new TaskServiceImpl(taskDataRepos);
       await taskService.updateTask(task);
       console.log("Updated:", task);
       return task; 
